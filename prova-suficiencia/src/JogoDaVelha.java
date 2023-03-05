b14269b7448deb31c7819fc0f8af6c69d5fcdd27
NikolasTrapp <nikolastrapp@gmail.com>
2023-03-05 11:30:50 -0300

    Arrumando questão da inicialização dos atributos da classe principal no método main

--- prova-suficiencia/src/JogoDaVelha.java
+++ prova-suficiencia/src/JogoDaVelha.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
 /**
- * Esta classe é a classe de entrada, ela inicializa as regras e gerencia o jogo.
+ * Esta classe é a classe de entrada, ela inicializa as regras e gerencia o
+ * jogo.
  */
 public class JogoDaVelha {
 
-    private static final JogoDaVelha_Mapa jogoMapa = new JogoDaVelha_Mapa();
-    private static final JogoDaVelha_PC jogoPC = new JogoDaVelha_PC(jogoMapa);
-    private static final JogoDaVelha_Jogador jogoJogador = new JogoDaVelha_Jogador(jogoMapa);
+    private static JogoDaVelha_Mapa jogoMapa;
+    private static JogoDaVelha_PC jogoPC;
+    private static JogoDaVelha_Jogador jogoJogador;
 
     /**
-     * Esta é a classe princpal, ela é a que inicia a aplicação.
+     * Esta é a função princpal, ela é a que inicia a aplicação.
      */
     public static void main(String[] args) throws Exception {
-        
+
+        jogoMapa = new JogoDaVelha_Mapa();
+        jogoPC = new JogoDaVelha_PC(jogoMapa);
+        jogoJogador = new JogoDaVelha_Jogador(jogoMapa);
+
         Scanner teclado = new Scanner(System.in);
         boolean flag = true;
         do {
             jogar(teclado);
             System.out.println("Deseja jogar novamente (s/n)?");
             char opc = teclado.next().charAt(0);
-            if (opc == 'n'){
+            if (opc == 'n') {
                 flag = false;
             } else {
                 jogoMapa.limpaMapa();
@@ -31,30 +36,31 @@ public class JogoDaVelha {
     /**
      * Esta função controla os rounds, enquanto ninguem ganhar ou empatar será
      * escolhido alguém para jogar.
+     * 
      * @param teclado
      */
-    private static void jogar(Scanner teclado){
+    private static void jogar(Scanner teclado) {
         int count = 0;
-        int aleatorio = (int)(Math.random() * 2);
+        int aleatorio = (int) (Math.random() * 2);
 
-        while (true){
+        while (true) {
             jogoMapa.desenha(count);
-            if (jogoMapa.empatou()){
+            if (jogoMapa.empatou()) {
                 System.out.println("... EMPATOU!");
                 break;
             }
-            if (aleatorio % 2 == 0){
-                while (!jogoJogador.joga(teclado)){
+            if (aleatorio % 2 == 0) {
+                while (!jogoJogador.joga(teclado)) {
                     System.out.println("Jogue novamente!");
                 }
-                if (jogoMapa.ganhou(jogoJogador.getLetra())){
+                if (jogoMapa.ganhou(jogoJogador.getLetra())) {
                     jogoMapa.desenha(++count);
                     System.out.println("...Jogador ganhou!");
                     break;
                 }
             } else {
                 jogoPC.joga();
-                if (jogoMapa.ganhou(jogoPC.getLetra())){
+                if (jogoMapa.ganhou(jogoPC.getLetra())) {
                     jogoMapa.desenha(++count);
                     System.out.println("...PC ganhou!");
                     break;
@@ -62,7 +68,7 @@ public class JogoDaVelha {
             }
             aleatorio++;
             count++;
-        }  
+        }
     }
 
 }
